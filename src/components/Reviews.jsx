553001ae7c8384619e332b375ee47af17a0dae24
muhsinaltintop <muhsin.altintop@gmail.com>
2022-02-17 15:40:17 +0300

    loading... and error added and styling for review is done

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,33 +1,46 @@
 import React, { useEffect, useState } from 'react'
 import { getAllReviews } from '../utils/api'
+import styles from "./Reviews.module.css";
 import dayjs from 'dayjs';
 
 const Reviews = () => {
   const [reviews, setAllReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
 
 useEffect(()=>{
+    setIsLoading(true);
+    setError(null);
     getAllReviews()
     .then((reviewsFromApi) => {
         setAllReviews(reviewsFromApi);
+        setIsLoading(false);
+    })
+    .catch((err)=>{
+        setError(err);
+    });
 
-    })  
-
-})  
+}, []);  
     return (
-    <div>
+    <main className={styles.review}>
         <h2>Reviews</h2>
+        { isLoading ? (
+            <p>Loading</p>
+        ) : error ? (
+            <p>Error!</p>
+        ) : (
         <ul>
             {reviews.map((review)=>{
                 return(
                     <li key={review.review_id}>
                         <img src={review.review_img_url} alt={review.title} />
-                        <div>{review.title}</div>
-                        <div>{dayjs(review.created_at).format("DD/MM/YYYY")}</div>
-                        <div>by {review.owner}</div>
-                        <div>Designer: {review.designer}</div>
-                        <div>Review: {review.review_body}</div>
-                        <div> Votes: {review.votes}</div>                                             
+                        <div className={styles.title}>{review.title}</div>
+                        <div className={styles.date}>{dayjs(review.created_at).format("DD/MM/YYYY")}</div>
+                        <div className={styles.owner}>by {review.owner}</div>
+                        <div  className={styles.designer}>Designer: {review.designer}</div>
+                        <div  className={styles.review_body}>Review: {review.review_body}</div>
+                        <div  className={styles.votes}> Votes: {review.votes}</div>                                             
                     </li>
 
                 )
@@ -38,7 +51,14 @@ useEffect(()=>{
 
             }
         </ul>
-        </div>
+
+        )
+
+        }
+    
+    
+    
+    </main>
   )
 }
 
