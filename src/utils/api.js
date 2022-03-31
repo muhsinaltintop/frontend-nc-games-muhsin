335ffb93eb492c282ac6a049110c7e03b4ad43c5
Muhsin Altıntop <muhsin.altintop@gmail.com>
2022-03-31 18:26:49 +0300

    timeout set

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,64 +1,65 @@
 import axios from "axios";
 
 const gamesApi = axios.create({
-    baseURL: "https://muhsinncgames.herokuapp.com/api",
+  baseURL: "https://muhsinncgames.herokuapp.com/api",
 });
 
 export const getAllReviews = () => {
-    return gamesApi.get(`/reviews`).then((res)=> {
-        return res.data.reviews;
-    })
+  return gamesApi.get(`/reviews`, {}, { timeout: 2 }).then((res) => {
+    return res.data.reviews;
+  });
 };
 
 export const getReview = (review_id) => {
-    return gamesApi.get(`/reviews/${review_id}`).then((res)=> {
-        return res.data.reviews;
-    })
+  return gamesApi.get(`/reviews/${review_id}`).then((res) => {
+    return res.data.reviews;
+  });
 };
 
 export const getAllCategories = () => {
-    return gamesApi.get(`/categories`).then((res)=> {
-        return res.data.categories;
-    })
+  return gamesApi.get(`/categories`).then((res) => {
+    return res.data.categories;
+  });
 };
 
 export const getUserByName = (username) => {
-    return gamesApi.get(`/users/${username}`).then((res)=> {
-        return res.data.user;
-    })
+  return gamesApi.get(`/users/${username}`).then((res) => {
+    return res.data.user;
+  });
 };
 
 export const getAllUsers = () => {
-    return gamesApi.get(`/users`).then((res)=> {
-        return res.data.users;
-    })
+  return gamesApi.get(`/users`).then((res) => {
+    return res.data.users;
+  });
 };
 
 export const getCommentsByReview = (review_id) => {
-    return gamesApi.get(`/reviews/${review_id}/comments`).then((res) => {
-      return res.data.comments;
-    });
-  };
+  return gamesApi.get(`/reviews/${review_id}/comments`).then((res) => {
+    return res.data.comments;
+  });
+};
 
 export const postCommentByReviewId = (review_id, username, body) => {
-    return gamesApi.post(`/reviews/${review_id}/comments`, {username: username, body: body}).then((res)=>{
+  return gamesApi
+    .post(`/reviews/${review_id}/comments`, { username: username, body: body })
+    .then((res) => {
       return res.data.comment;
-    })
-}
-  
-export const deleteCommentById = (comment_id) => {
-    return gamesApi.delete(`/comments/${comment_id}`).then((res)=>{
-    })
-}
-
-export const getReviewsByCategory = (category) => {
-    return gamesApi.get(`/reviews?category=${category}`).then((res) => {
-      return res.data.reviews;
     });
 };
-  
+
+export const deleteCommentById = (comment_id) => {
+  return gamesApi.delete(`/comments/${comment_id}`).then((res) => {});
+};
+
+export const getReviewsByCategory = (category) => {
+  return gamesApi.get(`/reviews?category=${category}`).then((res) => {
+    return res.data.reviews;
+  });
+};
+
 export const sortReviewsBy = (query) => {
-    return gamesApi.get(`/reviews?${query}`).then((res) => {
-      return res.data.reviews;
-    });
+  return gamesApi.get(`/reviews?${query}`).then((res) => {
+    return res.data.reviews;
+  });
 };
